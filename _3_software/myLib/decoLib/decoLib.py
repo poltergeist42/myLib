8b0dbda8664564cde3b534fdcdc18c21e18eace1
poltergeist42 <lepoltergeist@gmail.com>
2016-10-26 23:43:00 +0200

    WIP-decoLib : finalisation de la methode 'f_pbPercent' (ancienne f_progressPercent)

--- _3_software/myLib/decoLib/decoLib.py
+++ _3_software/myLib/decoLib/decoLib.py
@@ -8,7 +8,7 @@ Infos
 
    :Nom du fichier:     fakeLib.py
    :Autheur:            `Poltergeist42 <https://github.com/poltergeist42>`_
-   :Version:            20161023
+   :Version:            20161026
 
 ####
 
@@ -66,15 +66,19 @@ import time
 
 class C_ProgressBar( object ) :
     """ Class permettant de créer une barre de progression """
-    def __init__( self, v_char = '#', v_width = 100 ) :
+    def __init__( self, v_etiquette = "Progression", v_char = '#', v_width = 100 ) :
         """ **__init__()**
         
             Creation et initialisation des variables globales de cette Class
         """
         
         # declaration des variables
-        self.v_char = v_char
-        self.v_width = v_width
+        self.v_char             = v_char
+        self.v_width            = v_width
+        self.v_etiquette        = v_etiquette
+        self.v_percent          = 0
+        self.v_prevValue        = 0
+        self.v_firstStep        = False
 
 ####
         
@@ -101,35 +105,132 @@ class C_ProgressBar( object ) :
         
 ####
 
-    def f_progressPercent(self, v_valueMax, v_value = 0):
+    def f_pbPercent(self, v_valueMax, v_value = 0):
         """ **f_progress( int )**
         
-            Permet de déssiner la barre de progression
+            Permet de déssiner la barre de progression et d'afficher
+            le pourcentage d'avancement
         """
         
         ## dbg
         v_dbg = 1
         v_dbg2 = 1
-        f_dbg(v_dbg2, "f_progress", self.f_progress)
+        f_dbg(v_dbg2, "f_pbPercent", self.f_pbPercent)
         
         ## Action
-        spaces = ' ' * (self.v_width-1)
-        backspace = '\b' * (self.v_width+4)
-        v_percent = 0
-        print('Starting [{}] {:03}%{}'.format(spaces, v_percent, backspace), end='')
-        sys.stdout.flush()
-        steps = v_valueMax / self.v_width
-        for i in range(v_valueMax):
-            v_percent+=1
-            spaces = ' ' * (self.v_width - i )
-            backspace = '\b' * ((self.v_width+6)-i)
-            time.sleep(0.1)
-            if i % steps == 0:
-                if not v_percent == self.v_width :
-                    print("{}{}] {:03}%{}".format(self.v_char, spaces, v_percent, backspace), end='')
-                    sys.stdout.flush()
-                else :
-                    print("{}{}] {:03}%".format(self.v_char, spaces, v_percent), end='\n')
+        if not v_value :
+            v_ratio = False
+            
+        else :
+            if v_valueMax < self.v_width :
+                v_valueMax = v_valueMax * self.v_width + self.v_width //2
+                v_value = v_value * self.v_width + self.v_width + self.v_width //2
+                    # Si la valeur Max est trop petite, on multiplie la valeur Max
+                    # et la valeur à représenter par la longueur de la barre de progression
+                    # puis on ajoute la moitier de cette longueur
+
+            v_ratio = self.f_setRatio( v_valueMax, v_value)
+            
+        if not v_ratio and not self.v_firstStep :          
+            spaces = ' ' * (self.v_width-1)
+            backspace = '\b' * (self.v_width+4)
+            print(' {} [{}] {:03}%{}'.format(self.v_etiquette, spaces, 0, backspace), end='')
+            sys.stdout.flush()
+            self.v_firstStep = True
+            pass
+            
+        elif not v_ratio and self.v_firstStep :
+            pass
+            
+        else :
+            spaces = ' ' * (self.v_width - v_ratio )
+            backspace = '\b' * ((self.v_width+6))
+            time.sleep(0.05)
+            
+            if not v_ratio == self.v_width :
+                print("{}{}] {:03}%{}".format(self.v_char * v_ratio, spaces, v_ratio+1, backspace), end='')
+                sys.stdout.flush()
+            else :
+                print("{}{}] 100%".format(self.v_char * v_ratio, spaces), end='\n')
+####
+
+    def f_pbRatio(self, v_valueMax, v_value = 0):
+        """ **f_progress( int )**
+        
+            Permet de déssiner la barre de progression et d'afficher
+            l'avancement sous forme de rapport ::
+            
+                1/25
+                2/25
+                3/25
+                etc ...
+        """
+        
+        ## dbg
+        v_dbg = 1
+        v_dbg2 = 1
+        f_dbg(v_dbg2, "f_pbRatio", self.f_pbRatio)
+        
+        ## Action
+        v_valueMaxReal = v_valueMax
+        v_vMaxLen = len( str(v_valueMaxReal))
+        v_valueReal = v_value + 1
+        v_valueLen = len( str(v_valueReal) )
+        v_lenDiff = v_vMaxLen - v_valueLen
+        
+        if not v_value :
+            v_ratio = False
+            
+        else :
+            if v_valueMax < self.v_width :
+                v_valueMax = v_valueMax * self.v_width + self.v_width //2
+                v_value = v_value * self.v_width + self.v_width + self.v_width //2
+                    # Si la valeur Max est trop petite, on multiplie la valeur Max
+                    # et la valeur à représenter par la longueur de la barre de progression
+                    # puis on ajoute la moitier de cette longueur
+
+            v_ratio = self.f_setRatio( v_valueMax, v_value)
+            
+        if not v_ratio and not self.v_firstStep :          
+            spaces = ' ' * (self.v_width-1)
+            backspace = '\b' * (self.v_width+4)
+            print(" {} [{}] {}/{}{}".format (
+                                                self.v_etiquette,
+                                                spaces,
+                                                '0'* v_vMaxLen,
+                                                v_valueMaxReal,
+                                                backspace),
+                                                end=''
+                                            )
+            sys.stdout.flush()
+            self.v_firstStep = True
+            pass
+            
+        elif not v_ratio and self.v_firstStep :
+            pass
+            
+        else :
+            spaces = ' ' * ( self.v_width - v_ratio )
+            backspace = '\b' * ((self.v_width+1+v_valueMaxReal*2))
+            time.sleep(0.05)
+            
+            if not v_ratio == self.v_width :
+                print("{}{}] {}{}/{}{}".format  (   self.v_char * v_ratio,
+                                                    spaces,
+                                                    '0'* v_lenDiff, 
+                                                    v_valueReal,
+                                                    v_valueMaxReal,
+                                                    backspace),
+                                                    end=''
+                                                )
+                sys.stdout.flush()
+            else :
+                print("{}{}] {}/{}".format  (   self.v_char * v_ratio,
+                                                spaces,
+                                                v_valueMaxReal,
+                                                v_valueMaxReal),
+                                                end='\n'
+                                            )
 ####
 
     def f_setRatio( self, v_valueMax, v_value ) :
@@ -173,8 +274,10 @@ def main() :
             print( "Le mode Debug ne peut pas etre active car le module n'est pas present")
             
     if args.progress :
-        i_ist = C_ProgressBar()
-        i_ist.f_progressPercent( 150 )
+        i_ist = C_ProgressBar(  )
+        v_valueMax = 100
+        for i in range( v_valueMax ) :
+            i_ist.f_pbRatio( v_valueMax, i )
 
         
         del i_ist
